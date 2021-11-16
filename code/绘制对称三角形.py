e6cab066912f259c579f0e4f0dccd707ff80ea60
as7er <83108160+as7er@users.noreply.github.com>
2021-11-17 02:47:04 +0800

    Add files via upload

---
+++ code/绘制对称三角形.py
@@ -0,0 +1,15 @@
+import turtle	#导入turtle模块
+turtle.showturtle()	#显示箭头
+turtle.write("as7er")	#写字符串
+turtle.forward(300)	#前进300像素
+turtle.color("red")	#画笔颜色改成red
+turtle.left(90)	#箭头左转90度
+turtle.forward(300)	#前进300像素
+turtle.goto(0,0)	#箭头去到坐标原点
+turtle.color("black")	#画笔颜色改成black
+turtle.left(90)	#箭头左转90度
+turtle.forward(300)	#前进300像素
+turtle.right(90)	#箭头右转90度
+turtle.color("red")	#画笔颜色改成red
+turtle.forward(300)	#前进300像素
+turtle.goto(0,0)	#箭头去到坐标原点
